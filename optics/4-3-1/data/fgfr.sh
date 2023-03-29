53549ace6e8c1f5653e928fadcafef9857710aa4
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2023-03-29 15:16:02 +0300

    fix lab 4-3-1

--- optics/4-3-1/data/fgfr.sh
+++ optics/4-3-1/data/fgfr.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/gnuplot
 set terminal png size 1200, 1000
-set output "data/fgrf.png"
+set output "data/fgfr.png"
 set font "Times, 20"
 set style line 1 lc rgb "blue" lw 4
 set style line 2 lc rgb "red" lw 4 ps 3
@@ -9,7 +9,7 @@ set grid xtics ytics
 set xzeroaxis linestyle 3
 set yzeroaxis linestyle 3
 set xlabel "n" font "Times, 20"
-set ylabel "x_n" font "Times, 20"
+set ylabel "x_n, mm" font "Times, 20"
 set xrange [0.6 : 5.4]
-set yrange [-0.012 : 0.132]
-plot -0.0304 * x +0.1488 linestyle 1, "data/fgfr.txt" with points linestyle 2 notitle
+set yrange [-0.06 : 0.66]
+plot 0.152 * x -0.168 linestyle 1, "data/fgfr.txt" with points linestyle 2 notitle
