da30fae3577a1c1bf3214a5c2186e3ef81d34797
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2023-09-13 12:51:19 +0300

    upd

--- quantum-physics/1-2/data/1.sh
+++ quantum-physics/1-2/data/1.sh
@@ -8,8 +8,6 @@ set style line 3 lc rgb "black" lw 2
 set grid xtics ytics
 set xzeroaxis linestyle 3
 set yzeroaxis linestyle 3
-set xlabel "1-cos\theta" font "Times, 20"
+set xlabel "1-cos theta" font "Times, 20"
 set ylabel "1/N" font "Times, 20"
-set xrange [-0.15 : 1.65]
-set yrange [-0.0005257 : 0.0039347]
-plot 0.00243862 * x +2.35952e-05 linestyle 1, "data/1.txt" with points linestyle 2 notitle
+plot "data/1.txt" with xyerrorbars, 0.00244694 * x +0.00171507 linestyle 1
